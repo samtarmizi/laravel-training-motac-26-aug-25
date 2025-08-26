7cb5d79ffa8f0eba183d960f703dda05b6344cd9
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2025-08-26 16:31:37 +0800

    index table

--- resources/views/inventories/index.blade.php
+++ resources/views/inventories/index.blade.php
@@ -8,7 +8,25 @@
                 <div class="card-header">{{ __('Inventories Index') }}</div>
 
                 <div class="card-body">
-                    
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Quantity</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($inventories as $inventory)
+                                <tr>
+                                    <td>{{ $inventory->id }}</td>
+                                    <td>{{ $inventory->name }}</td>
+                                    <td>{{ $inventory->quantity }}</td>  
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
