3fa3339b0cd815b120bc598e21cc2da20b5f8299
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2025-09-11 11:48:45 +0800

    chore: fix ui from themes default lorem ipsum

--- resources/views/inventories/create.blade.php
+++ resources/views/inventories/create.blade.php
@@ -1,7 +1,11 @@
-@extends('layouts.app')
+@extends('admin.layouts.main')
 
 @section('content')
-<div class="container">
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Dashboard</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">Dashboard</li>
+    </ol>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
